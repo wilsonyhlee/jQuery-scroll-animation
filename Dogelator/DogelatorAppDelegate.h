875edb49b31a2bb31b96a1807534c614c30c7487
wilsonyhlee <gowaneil@gmail.com>
2014-04-18 17:12:23 -0700

    initial push

---
+++ Dogelator/DogelatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DogelatorAppDelegate.h
+//  Dogelator
+//
+//  Created by Trent Underwood on 4/16/14.
+//  Copyright (c) 2014 Trent Underwood & Wilson Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DogelatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dogelator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dogelator
+//
+//  Created by Trent Underwood on 4/16/14.
+//  Copyright (c) 2014 Trent Underwood & Wilson Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DogelatorAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DogelatorAppDelegate class]));
+    }
+}
